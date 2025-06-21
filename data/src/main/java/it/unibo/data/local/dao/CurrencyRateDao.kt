fbdd789b73da89b64ae973b6af26554f17ad7623
Alex <alex.cambrini@studio.unibo.it>
2025-06-21 13:59:30 +0200

    feat: define CurrencyRateEntity and CurrencyRateDao for local Room storage

---
+++ data/src/main/java/it/unibo/data/local/dao/CurrencyRateDao.kt
@@ -0,0 +1,20 @@
+package it.unibo.data.local.dao
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import it.unibo.data.local.entity.CurrencyRateEntity
+
+interface CurrencyRateDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRates(rates: List<CurrencyRateEntity>)
+
+    @Query("SELECT * FROM currency_rates WHERE base = :base")
+    suspend fun getRatesByBase(base: String): List<CurrencyRateEntity>
+
+    @Query("SELECT rate FROM currency_rates WHERE base = :base AND currencyCode = :toCurrency LIMIT 1")
+    suspend fun getRate(base: String, toCurrency: String): Double?
+
+    @Query("DELETE FROM currency_rates WHERE timestamp < :expiryTime")
+    suspend fun deleteOldRates(expiryTime: Long)
+}
---
+++ data/src/main/java/it/unibo/data/local/entity/CurrencyRateEntity.kt
@@ -0,0 +1,14 @@
+package it.unibo.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "currency_rates")
+data class CurrencyRateEntity (
+    @PrimaryKey
+    val currencyCode: String,
+    val rate: Double,
+    val base: String,
+    val timestamp: Long
+)
