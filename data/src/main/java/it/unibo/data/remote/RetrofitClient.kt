73fd1bf01a40613972a47ff540d23d4e1595b7c7
Alex <alex.cambrini@studio.unibo.it>
2025-06-21 18:56:38 +0200

    refactor: change RetrofitClient from object to class - converted RetrofitClient singleton object to a regular class - replaced constant BASE_URL with private val baseUrl - no changes in functionality, allows instance creation

--- data/src/main/java/it/unibo/data/remote/RetrofitClient.kt
+++ data/src/main/java/it/unibo/data/remote/RetrofitClient.kt
@@ -5,14 +5,14 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-object RetrofitClient {
-    private const val BASE_URL = "https://api.frankfurter.app/"
+class RetrofitClient {
+    private val baseUrl = "https://api.frankfurter.app/"
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(baseUrl)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
