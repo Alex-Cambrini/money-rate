c3457066aa20319153a81b0bfd19e30fe1240441
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-22 19:14:39 +0200

    Add WalletRepository interface for wallet entry management

---
+++ domain/src/main/java/it/unibo/domain/repository/WalletRepository.kt
@@ -0,0 +1,10 @@
+package it.unibo.domain.repository
+
+import it.unibo.domain.model.WalletEntry
+import kotlinx.coroutines.flow.Flow
+
+interface WalletRepository {
+    suspend fun addEntry(entry: WalletEntry)
+    suspend fun removeEntry(entry: WalletEntry)
+    fun getAllEntries(): Flow<List<WalletEntry>>
+}
