c6e1dd0249c1c6f388a2f87a3ad0b5973ddad888
Alex <alex.cambrini@studio.unibo.it>
2025-06-22 11:47:15 +0200

    refactor: update MainActivity to initialize RepositoryProviderImpl and display HomeScreen with repository data

--- composeui/src/main/java/it/unibo/composeui/MainActivity.kt
+++ composeui/src/main/java/it/unibo/composeui/MainActivity.kt
@@ -3,12 +3,19 @@ package it.unibo.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import it.unibo.composeui.screens.HomeScreen
+import it.unibo.data.di.RepositoryProviderImpl
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var repositoryProvider: RepositoryProviderImpl
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        repositoryProvider = RepositoryProviderImpl(this)
+
         setContent {
-            MainActivity()
+            HomeScreen(repositoryProvider.currencyRepository)
         }
     }
 }
