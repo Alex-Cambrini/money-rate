49b2f1ef6bc359e54b6ca347f6bb5e781aeb818b
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-22 19:13:44 +0200

    Add walletRepository implementation to RepositoryProviderImpl

--- data/src/main/java/it/unibo/data/di/RepositoryProviderImpl.kt
+++ data/src/main/java/it/unibo/data/di/RepositoryProviderImpl.kt
@@ -6,15 +6,23 @@ import it.unibo.domain.di.RepositoryProvider
 import it.unibo.domain.repository.CurrencyRepository
 import it.unibo.data.remote.RetrofitClient
 import it.unibo.data.repository.CurrencyRepositoryImpl
+import it.unibo.domain.repository.WalletRepository
+import it.unibo.data.repository.WalletRepositoryImpl
 
 class RepositoryProviderImpl(private val context: Context) : RepositoryProvider {
 
     private val retrofitClient = RetrofitClient()
-    private val database = AppDatabase.getInstance(context) // esempio database Room
+    private val database = AppDatabase.getInstance(context)
     private val currencyRateDao = database.currencyRateDao()
+    private val walletDao = database.walletDao()
 
     override var currencyRepository: CurrencyRepository = CurrencyRepositoryImpl(
         dao = currencyRateDao,
         api = retrofitClient.currencyApi
     )
+
+    override var walletRepository: WalletRepository = WalletRepositoryImpl(
+        dao = walletDao
+    )
 }
+
