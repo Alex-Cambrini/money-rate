d8414ec9970122330698603e8de84f5952a1643f
Alex <alex.cambrini@studio.unibo.it>
2025-06-22 11:50:45 +0200

    refactor: complete ConvertCurrencyUseCase implementation

--- domain/src/main/java/it/unibo/domain/usecase/ConvertCurrencyUseCase.kt
+++ domain/src/main/java/it/unibo/domain/usecase/ConvertCurrencyUseCase.kt
@@ -1,7 +1,16 @@
 package it.unibo.domain.usecase
 
 import it.unibo.domain.repository.CurrencyRepository
+import it.unibo.domain.model.CurrencyRate
 
 interface ConvertCurrencyUseCase {
     suspend fun invoke(from: String, to: String): CurrencyRate
+}
+class ConvertCurrencyUseCaseImpl (
+    private val currencyRepository: CurrencyRepository
+): ConvertCurrencyUseCase {
+    override suspend fun invoke(from: String, to: String): CurrencyRate {
+        val rate = currencyRepository.getRate(from, to)
+        return CurrencyRate(from, to, rate)
+    }
 }
