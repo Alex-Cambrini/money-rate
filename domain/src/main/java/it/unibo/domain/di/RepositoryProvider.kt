4f1c07f3a609db1248b8bedde85c4d6f2022ac11
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-22 19:13:28 +0200

    Add walletRepository to RepositoryProvider interface

--- domain/src/main/java/it/unibo/domain/di/RepositoryProvider.kt
+++ domain/src/main/java/it/unibo/domain/di/RepositoryProvider.kt
@@ -1,7 +1,9 @@
 package it.unibo.domain.di
 
 import it.unibo.domain.repository.CurrencyRepository
+import it.unibo.domain.repository.WalletRepository
 
 interface RepositoryProvider {
     val currencyRepository: CurrencyRepository
+    val walletRepository: WalletRepository
 }
