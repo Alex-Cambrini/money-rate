ffaabfce36ba57b1306a92233503e4fc668214c0
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-22 19:14:26 +0200

    Add WalletDao interface for wallet entry operations

---
+++ data/src/main/java/it/unibo/data/local/dao/WalletDao.kt
@@ -0,0 +1,18 @@
+package it.unibo.data.local.dao
+
+import androidx.room.*
+import it.unibo.data.local.entity.WalletEntryEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WalletDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entry: WalletEntryEntity)
+
+    @Delete
+    suspend fun delete(entry: WalletEntryEntity)
+
+    @Query("SELECT * FROM wallet")
+    fun getAll(): Flow<List<WalletEntryEntity>>
+}
