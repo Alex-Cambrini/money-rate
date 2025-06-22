8b061e73f27fb5d422a2a9bb4498c0f58f2fea39
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-22 19:13:19 +0200

    Refactor CurrencyUpdateWorker to use RepositoryProviderImpl for CurrencyRepository

--- app/src/main/java/it/unibo/moneyrate/worker/CurrencyUpdateWorker.kt
+++ app/src/main/java/it/unibo/moneyrate/worker/CurrencyUpdateWorker.kt
@@ -3,10 +3,8 @@ package it.unibo.moneyrate.worker
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import it.unibo.data.di.RepositoryProviderImpl
 import it.unibo.domain.repository.CurrencyRepository
-import it.unibo.data.repository.CurrencyRepositoryImpl
-import it.unibo.moneyrate.network.RetrofitInstance
-import it.unibo.data.local.db.AppDatabase
 
 class CurrencyUpdateWorker(
     private val context: Context,
@@ -14,9 +12,8 @@ class CurrencyUpdateWorker(
 ) : CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
-        val dao = AppDatabase.getInstance(context).currencyRateDao()
-        val api = RetrofitInstance.api
-        val repository: CurrencyRepository = CurrencyRepositoryImpl(dao, api)
+        val repository: CurrencyRepository =
+            RepositoryProviderImpl(context).currencyRepository
 
         return try {
             repository.getAllRates("EUR")
