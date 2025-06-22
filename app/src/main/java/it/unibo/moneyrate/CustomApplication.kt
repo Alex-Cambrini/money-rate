501dd670fc0fa9d5d639b8eda201624a026938ab
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-22 18:07:57 +0200

    Add periodic currency updates using WorkManager

--- app/src/main/java/it/unibo/moneyrate/CustomApplication.kt
+++ app/src/main/java/it/unibo/moneyrate/CustomApplication.kt
@@ -1,16 +1,31 @@
 package it.unibo.moneyrate
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import it.unibo.data.di.RepositoryProviderImpl
 import it.unibo.domain.di.UseCaseProvider
+import it.unibo.moneyrate.worker.CurrencyUpdateWorker
+import java.util.concurrent.TimeUnit
 
-class CustomApplication: Application() {
+class CustomApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         UseCaseProvider.setup(
             repositoryProvider = RepositoryProviderImpl(context = this.applicationContext)
+        )
+
+        val workRequest = PeriodicWorkRequestBuilder<CurrencyUpdateWorker>(
+            3, TimeUnit.HOURS
+        ).build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "CurrencyUpdate",
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
         )
     }
 }
