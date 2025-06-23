5935dbd3b18fd2b02802d73bb337b7d5bd75581e
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-23 08:53:10 +0200

    Add splash screen to MainActivity before displaying home screen

--- composeui/src/main/java/it/unibo/composeui/MainActivity.kt
+++ composeui/src/main/java/it/unibo/composeui/MainActivity.kt
@@ -3,7 +3,9 @@ package it.unibo.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
 import it.unibo.composeui.screens.HomeScreen
+import it.unibo.composeui.screens.SplashScreen
 import it.unibo.data.di.RepositoryProviderImpl
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +17,16 @@ class MainActivity : ComponentActivity() {
         repositoryProvider = RepositoryProviderImpl(this)
 
         setContent {
-            HomeScreen(repositoryProvider.currencyRepository)
+            var showSplash by remember { mutableStateOf(true) }
+
+            if (showSplash) {
+                SplashScreen {
+                    showSplash = false
+                }
+            }
+            else {
+                HomeScreen(repositoryProvider.currencyRepository)
+            }
         }
     }
 }
