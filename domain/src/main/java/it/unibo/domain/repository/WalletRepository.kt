1e7a0a8a157fc6fd9f7c27a9e756802affa7dbc0
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-24 12:26:42 +0200

    feat: add updateEntry method to WalletRepository interface

--- domain/src/main/java/it/unibo/domain/repository/WalletRepository.kt
+++ domain/src/main/java/it/unibo/domain/repository/WalletRepository.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface WalletRepository {
     suspend fun addEntry(entry: WalletEntry)
+    suspend fun updateEntry(entry: WalletEntry)
     suspend fun removeEntry(entry: WalletEntry)
     fun getAllEntries(): Flow<List<WalletEntry>>
 }
