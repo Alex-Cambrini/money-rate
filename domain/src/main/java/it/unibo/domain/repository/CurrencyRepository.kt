a48e61c7a1aac085bd533382884b3f89e1f25fff
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-24 12:25:52 +0200

    feat: add method to retrieve all currency rates to Euro

--- domain/src/main/java/it/unibo/domain/repository/CurrencyRepository.kt
+++ domain/src/main/java/it/unibo/domain/repository/CurrencyRepository.kt
@@ -2,6 +2,7 @@ package it.unibo.domain.repository
 
 interface CurrencyRepository {
     suspend fun getRate(from: String, to: String): Double?
+    suspend fun getAllRatesToEuro(): Map<String, Double>
     suspend fun refreshCache(): Boolean
     suspend fun getAvailableCurrencies(): List<String>
 }
