4a9de47a99072674a490519c8f5730e5f97757b1
Alex <alex.cambrini@studio.unibo.it>
2025-06-24 10:12:15 +0200

    add networkChecker to RepositoryProvider: - import NetworkChecker - add networkChecker property to interface

--- domain/src/main/java/it/unibo/domain/di/RepositoryProvider.kt
+++ domain/src/main/java/it/unibo/domain/di/RepositoryProvider.kt
@@ -1,9 +1,11 @@
 package it.unibo.domain.di
 
+import it.unibo.domain.NetworkChecker
 import it.unibo.domain.repository.CurrencyRepository
 import it.unibo.domain.repository.WalletRepository
 
 interface RepositoryProvider {
     val currencyRepository: CurrencyRepository
     val walletRepository: WalletRepository
+    val networkChecker: NetworkChecker
 }
