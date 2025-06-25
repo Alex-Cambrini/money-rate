28b0de5689913053fe5ec013a0617323ef9e63a2
Alex <alex.cambrini@studio.unibo.it>
2025-06-25 16:42:09 +0200

    refactor: format WalletDao imports and annotations

--- data/src/main/java/it/unibo/data/local/dao/WalletDao.kt
+++ data/src/main/java/it/unibo/data/local/dao/WalletDao.kt
@@ -1,6 +1,11 @@
 package it.unibo.data.local.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import it.unibo.data.local.entity.WalletEntryEntity
 import kotlinx.coroutines.flow.Flow
 
