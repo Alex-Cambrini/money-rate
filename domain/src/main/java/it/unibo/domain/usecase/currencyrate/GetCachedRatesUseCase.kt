49ec94b135e1166bc5a199099db47ebeb6ceb1f5
Alex <alex.cambrini@studio.unibo.it>
2025-06-25 16:52:53 +0200

    feat: add GetCachedRatesUseCase interface and implementation

--- domain/src/main/java/it/unibo/domain/usecase/currencyrate/GetCachedRatesUseCase.kt
+++ domain/src/main/java/it/unibo/domain/usecase/currencyrate/GetCachedRatesUseCase.kt
@@ -1,2 +1,15 @@
 package it.unibo.domain.usecase.currencyrate
 
+import it.unibo.domain.repository.CurrencyRateRepository
+
+interface GetCachedRatesUseCase {
+    suspend fun invoke(): Map<String, Double>
+}
+
+class GetCachedRatesUseCaseImpl(
+    private val currencyRateRepository: CurrencyRateRepository
+): GetCachedRatesUseCase {
+    override suspend fun invoke(): Map<String, Double> {
+        return currencyRateRepository.getCachedRates()
+    }
+}
