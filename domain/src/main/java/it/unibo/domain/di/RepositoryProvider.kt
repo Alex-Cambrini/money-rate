916e2b7fbcea0c081c70621b195b6342eb11531e
Alex <alex.cambrini@studio.unibo.it>
2025-06-25 14:54:36 +0200

    Refactor CurrencyRepository and RepositoryProvider interface

--- domain/src/main/java/it/unibo/domain/di/RepositoryProvider.kt
+++ domain/src/main/java/it/unibo/domain/di/RepositoryProvider.kt
@@ -1,11 +1,13 @@
 package it.unibo.domain.di
 
 import it.unibo.domain.NetworkChecker
+import it.unibo.domain.repository.CurrencyRateRepository
 import it.unibo.domain.repository.CurrencyRepository
 import it.unibo.domain.repository.WalletRepository
 
 interface RepositoryProvider {
     val currencyRepository: CurrencyRepository
+    val currencyRateRepository : CurrencyRateRepository
     val walletRepository: WalletRepository
     val networkChecker: NetworkChecker
 }
--- domain/src/main/java/it/unibo/domain/repository/CurrencyRepository.kt
+++ domain/src/main/java/it/unibo/domain/repository/CurrencyRepository.kt
@@ -1,8 +1,5 @@
 package it.unibo.domain.repository
 
 interface CurrencyRepository {
-    suspend fun getRate(from: String, to: String): Double?
-    suspend fun getCachedRates(): Map<String, Double>
-    suspend fun refreshCache(): Boolean
     suspend fun getAvailableCurrencies(): Map<String, String>
 }
