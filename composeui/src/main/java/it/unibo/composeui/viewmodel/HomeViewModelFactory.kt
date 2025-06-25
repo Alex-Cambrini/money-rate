e19deedf2b508cdde8a17f0233afc68f3b0d438f
Alex <alex.cambrini@studio.unibo.it>
2025-06-25 16:40:01 +0200

    refactor HomeViewModelFactory to use usecases instead of repository: - replace CurrencyRepository with ConvertCurrencyUseCase and GetAvailableCurrenciesUseCase - update ViewModel instantiation accordingly

--- composeui/src/main/java/it/unibo/composeui/viewmodel/HomeViewModelFactory.kt
+++ composeui/src/main/java/it/unibo/composeui/viewmodel/HomeViewModelFactory.kt
@@ -2,13 +2,17 @@ package it.unibo.composeui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import it.unibo.domain.repository.CurrencyRepository
+import it.unibo.domain.usecase.currencyrate.ConvertCurrencyUseCase
+import it.unibo.domain.usecase.currency.GetAvailableCurrenciesUseCase
 
-class HomeViewModelFactory(private val repository: CurrencyRepository) : ViewModelProvider.Factory {
+class HomeViewModelFactory(
+    private val convertCurrencyUseCase: ConvertCurrencyUseCase,
+    private val getAvailableCurrenciesUseCase: GetAvailableCurrenciesUseCase
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return HomeViewModel(repository) as T
+            return HomeViewModel(convertCurrencyUseCase, getAvailableCurrenciesUseCase) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
