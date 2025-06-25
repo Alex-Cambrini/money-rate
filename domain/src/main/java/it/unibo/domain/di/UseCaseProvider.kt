43fa71b6cc293e91786f8f1ad85336d8b6308fbd
Alex <alex.cambrini@studio.unibo.it>
2025-06-25 14:55:03 +0200

    Update UseCaseProvider and ConvertCurrencyUseCase implementation

--- domain/src/main/java/it/unibo/domain/di/UseCaseProvider.kt
+++ domain/src/main/java/it/unibo/domain/di/UseCaseProvider.kt
@@ -2,12 +2,18 @@ package it.unibo.domain.di
 
 import it.unibo.domain.usecase.ConvertCurrencyUseCase
 import it.unibo.domain.usecase.ConvertCurrencyUseCaseImpl
+import it.unibo.domain.usecase.GetAvailableCurrenciesUseCase
+import it.unibo.domain.usecase.GetAvailableCurrenciesUseCaseImpl
 
 object UseCaseProvider {
     lateinit var convertCurrencyUseCase: ConvertCurrencyUseCase
+    lateinit var getAvailableCurrenciesUseCase: GetAvailableCurrenciesUseCase
 
     fun setup(repositoryProvider: RepositoryProvider) {
         convertCurrencyUseCase = ConvertCurrencyUseCaseImpl(
+            currencyRepository = repositoryProvider.currencyRateRepository
+        )
+        getAvailableCurrenciesUseCase = GetAvailableCurrenciesUseCaseImpl(
             currencyRepository = repositoryProvider.currencyRepository
         )
     }
--- domain/src/main/java/it/unibo/domain/usecase/ConvertCurrencyUseCase.kt
+++ domain/src/main/java/it/unibo/domain/usecase/ConvertCurrencyUseCase.kt
@@ -1,13 +1,13 @@
 package it.unibo.domain.usecase
 
-import it.unibo.domain.repository.CurrencyRepository
+import it.unibo.domain.repository.CurrencyRateRepository
 import it.unibo.domain.model.CurrencyRate
 
 interface ConvertCurrencyUseCase {
     suspend fun invoke(from: String, to: String): CurrencyRate
 }
 class ConvertCurrencyUseCaseImpl (
-    private val currencyRepository: CurrencyRepository
+    private val currencyRepository: CurrencyRateRepository
 ): ConvertCurrencyUseCase {
     override suspend fun invoke(from: String, to: String): CurrencyRate {
         val rate = currencyRepository.getRate(from, to) ?: 0.0
