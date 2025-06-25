2d5d4a0ea336cbc45cba2f42131754443d0f196a
Alex <alex.cambrini@studio.unibo.it>
2025-06-25 17:20:41 +0200

    refactor: rename ConvertCurrencyUseCase to GetRateUseCase for clarity

--- domain/src/main/java/it/unibo/domain/usecase/currencyrate/GetRateUseCase.kt
+++ domain/src/main/java/it/unibo/domain/usecase/currencyrate/GetRateUseCase.kt
@@ -3,13 +3,13 @@ package it.unibo.domain.usecase.currencyrate
 import it.unibo.domain.model.CurrencyRate
 import it.unibo.domain.repository.CurrencyRateRepository
 
-interface ConvertCurrencyUseCase {
+interface GetRateUseCase {
     suspend fun invoke(from: String, to: String): CurrencyRate
 }
 
-class ConvertCurrencyUseCaseImpl (
+class GetRateUseCaseImpl (
     private val currencyRateRepository: CurrencyRateRepository
-): ConvertCurrencyUseCase {
+): GetRateUseCase {
     override suspend fun invoke(from: String, to: String): CurrencyRate {
         val rate = currencyRateRepository.getRate(from, to) ?: 0.0
         return CurrencyRate(from, to, rate)
