4a26ac4e8af9317b3786c9195c3ad8f37f519f76
Alex <alex.cambrini@studio.unibo.it>
2025-06-25 15:21:07 +0200

    fix: update RepositoryProviderImpl to include both CurrencyRepository and CurrencyRateRepository

--- data/src/main/java/it/unibo/data/di/RepositoryProviderImpl.kt
+++ data/src/main/java/it/unibo/data/di/RepositoryProviderImpl.kt
@@ -6,10 +6,12 @@ import it.unibo.data.local.db.AppDatabase
 import it.unibo.domain.di.RepositoryProvider
 import it.unibo.domain.repository.CurrencyRepository
 import it.unibo.data.remote.RetrofitClient
+import it.unibo.data.repository.CurrencyRateRepositoryImpl
 import it.unibo.data.repository.CurrencyRepositoryImpl
 import it.unibo.domain.repository.WalletRepository
 import it.unibo.data.repository.WalletRepositoryImpl
 import it.unibo.domain.NetworkChecker
+import it.unibo.domain.repository.CurrencyRateRepository
 
 class RepositoryProviderImpl(private val context: Context) : RepositoryProvider {
 
@@ -19,8 +21,11 @@ class RepositoryProviderImpl(private val context: Context) : RepositoryProvider 
     private val walletDao = database.walletDao()
 
     override var currencyRepository: CurrencyRepository = CurrencyRepositoryImpl(
-        dao = currencyRateDao,
         api = retrofitClient.currencyApi
+    )
+    override val currencyRateRepository: CurrencyRateRepository = CurrencyRateRepositoryImpl(
+        dao = currencyRateDao,
+        api = retrofitClient.currencyRateApi
     )
 
     override var walletRepository: WalletRepository = WalletRepositoryImpl(
