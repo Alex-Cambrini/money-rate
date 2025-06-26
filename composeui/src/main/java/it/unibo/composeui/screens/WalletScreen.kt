e250168cc4bb11300f698521bbfd7a778c739f7d
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-06-26 13:25:31 +0200

    refactor: add background modifier to ExposedDropdownMenu in WalletScreen

--- composeui/src/main/java/it/unibo/composeui/screens/WalletScreen.kt
+++ composeui/src/main/java/it/unibo/composeui/screens/WalletScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
@@ -387,7 +388,8 @@ fun DropdownMenuCurrencySelector(
         )
         ExposedDropdownMenu(
             expanded = expanded,
-            onDismissRequest = { expanded = false }
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.background(Color.White)
         ) {
             currencies.forEach { currency ->
                 DropdownMenuItem(
