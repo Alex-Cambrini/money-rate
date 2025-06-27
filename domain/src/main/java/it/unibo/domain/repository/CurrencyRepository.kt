7b3aa2a2889bbe5196482495724a082c8bf287e8
Alex <alex.cambrini@studio.unibo.it>
2025-06-27 15:30:57 +0200

    refactor change getAvailableCurrencies return type

--- domain/src/main/java/it/unibo/domain/repository/CurrencyRepository.kt
+++ domain/src/main/java/it/unibo/domain/repository/CurrencyRepository.kt
@@ -1,5 +1,7 @@
 package it.unibo.domain.repository
 
+import it.unibo.domain.model.Currency
+
 interface CurrencyRepository {
-    suspend fun getAvailableCurrencies(): Map<String, String>
+    suspend fun getAvailableCurrencies(): List<Currency>
 }
