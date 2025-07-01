ae41eb3d86df01ae4259c8a8c983c5901c2b4bd2
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-01 15:12:56 +0200

    refactor: remove unused import from CustomApplication.kt

--- app/src/main/java/it/unibo/moneyrate/CustomApplication.kt
+++ app/src/main/java/it/unibo/moneyrate/CustomApplication.kt
@@ -1,7 +1,6 @@
 package it.unibo.moneyrate
 
 import android.app.Application
-import android.util.Log
 import androidx.work.Configuration
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
