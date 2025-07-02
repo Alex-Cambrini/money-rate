0de6b8f70d4cc524beec5180735fda266e39625e
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-02 11:19:07 +0200

    refactor: remove splash screen logic from MainActivity

--- composeui/src/main/java/it/unibo/composeui/MainActivity.kt
+++ composeui/src/main/java/it/unibo/composeui/MainActivity.kt
@@ -3,30 +3,16 @@ package it.unibo.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import it.unibo.composeui.screens.MainScreen
-import it.unibo.composeui.screens.SplashScreen
 import it.unibo.composeui.theme.MoneyRateTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             MoneyRateTheme {
-                var showSplash by rememberSaveable { mutableStateOf(true) }
-
-                if (showSplash) {
-                    SplashScreen {
-                        showSplash = false
-                    }
-                } else {
-                    MainScreen()
-                }
+                MainScreen()
             }
         }
     }
