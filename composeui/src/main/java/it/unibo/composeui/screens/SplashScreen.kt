3dbd6c17088a25ec496bd1fd41287f6a7f334e93
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-02 11:20:05 +0200

    refactor: clean up SplashScreen by removing unused imports and code

--- composeui/src/main/java/it/unibo/composeui/screens/SplashScreen.kt
+++ composeui/src/main/java/it/unibo/composeui/screens/SplashScreen.kt
@@ -10,14 +10,12 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import it.unibo.composeui.R
 import it.unibo.composeui.theme.Dimens
-import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen() {
