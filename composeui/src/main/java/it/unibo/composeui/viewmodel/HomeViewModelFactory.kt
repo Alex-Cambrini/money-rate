1d1408871aa81714f30eb6cfbf6d05518cd14bff
Alex <alex.cambrini@studio.unibo.it>
2025-07-18 10:57:01 +0200

    refactor: simplify HomeViewModelFactory to receive use cases via constructor - remove internal creation of use cases inside factory

--- composeui/src/main/java/it/unibo/composeui/viewmodel/HomeViewModelFactory.kt
+++ composeui/src/main/java/it/unibo/composeui/viewmodel/HomeViewModelFactory.kt
@@ -2,26 +2,19 @@ package it.unibo.composeui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import it.unibo.domain.usecase.currency.GetAvailableCurrenciesUseCase
-import it.unibo.domain.usecase.currencyrate.GetRateUseCase
 import it.unibo.domain.usecase.home.CalculateConversionUseCase
 import it.unibo.domain.usecase.home.CalculateTopRatesUseCase
 import it.unibo.domain.usecase.home.GetSingleRateUseCase
 import it.unibo.domain.usecase.home.LoadHomeDataUseCase
 
 class HomeViewModelFactory(
-    private val getRateUseCase: GetRateUseCase,
-    private val getAvailableCurrenciesUseCase: GetAvailableCurrenciesUseCase
+    private val loadHomeDataUseCase: LoadHomeDataUseCase,
+    private val calculateTopRatesUseCase: CalculateTopRatesUseCase,
+    private val calculateConversionUseCase: CalculateConversionUseCase,
+    private val getSingleRateUseCase: GetSingleRateUseCase
 ) : ViewModelProvider.Factory {
-
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            val loadHomeDataUseCase =
-                LoadHomeDataUseCase(getAvailableCurrenciesUseCase, getRateUseCase)
-            val calculateTopRatesUseCase = CalculateTopRatesUseCase()
-            val calculateConversionUseCase = CalculateConversionUseCase()
-            val getSingleRateUseCase = GetSingleRateUseCase(getRateUseCase)
-
             @Suppress("UNCHECKED_CAST")
             return HomeViewModel(
                 loadHomeDataUseCase,
@@ -33,4 +26,5 @@ class HomeViewModelFactory(
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
+
 
