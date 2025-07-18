5c4048745d37646a2abacf5aa1cac63562222898
Alex <alex.cambrini@studio.unibo.it>
2025-07-18 14:02:41 +0200

    feat: add battery optimization ignore request in MainActivity onCreate

--- composeui/src/main/java/it/unibo/composeui/MainActivity.kt
+++ composeui/src/main/java/it/unibo/composeui/MainActivity.kt
@@ -1,6 +1,11 @@
 package it.unibo.composeui
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
@@ -28,6 +33,13 @@ import it.unibo.domain.usecase.home.LoadHomeDataUseCase
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val pm = getSystemService(Context.POWER_SERVICE) as PowerManager
+        if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+            val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+        }
 
         setContent {
             MoneyRateTheme {
