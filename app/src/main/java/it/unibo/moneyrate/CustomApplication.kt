b3c3ce467fa842103e3abcddcbcd43658c09dc52
Lore-Ferra <lorenzo.ferrari27@studio.unibo.it>
2025-07-18 15:18:26 +0200

    feat: add network constraints and backoff policy to CurrencyUpdateWorker

--- app/src/main/java/it/unibo/moneyrate/CustomApplication.kt
+++ app/src/main/java/it/unibo/moneyrate/CustomApplication.kt
@@ -1,10 +1,14 @@
 package it.unibo.moneyrate
 
 import android.app.Application
+import androidx.work.BackoffPolicy
 import androidx.work.Configuration
+import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import androidx.work.WorkRequest
 import it.unibo.data.di.RepositoryProviderImpl
 import it.unibo.domain.di.UseCaseProvider
 import it.unibo.moneyrate.worker.CurrencyUpdateWorker
@@ -27,10 +31,24 @@ class CustomApplication : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         UseCaseProvider.setup(repositoryProvider)
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val workRequest = PeriodicWorkRequestBuilder<CurrencyUpdateWorker>(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .setBackoffCriteria(
+                BackoffPolicy.EXPONENTIAL,
+                WorkRequest.MIN_BACKOFF_MILLIS,
+                TimeUnit.MILLISECONDS
+            )
+            .build()
+
         WorkManager.getInstance(this).enqueueUniquePeriodicWork(
             "CurrencyUpdate",
             ExistingPeriodicWorkPolicy.KEEP,
-            PeriodicWorkRequestBuilder<CurrencyUpdateWorker>(15, TimeUnit.MINUTES).build()
+            workRequest
         )
     }
 }
